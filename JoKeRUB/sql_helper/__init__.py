564d3b924fe8e7f88106520fb99ca8724ae17b06
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-18 23:33:31 +0300

    Update __init__.py

--- JoKeRUB/sql_helper/__init__.py
+++ JoKeRUB/sql_helper/__init__.py
@@ -1,6 +1,5 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine                                               from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 # the secret configuration specific things
@@ -8,18 +7,18 @@ from ..Config import Config
 from ..core.logger import logging
 
 LOGS = logging.getLogger(__name__)
-
+engine = None
 BASE = declarative_base()
 
 def start() -> scoped_session:
+    global engine
     engine = create_engine(Config.DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
 try:
-    SESSION = start()
+    SESSION = start()                                                              
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
-    LOGS.error("DB_URI is not configured. Features depending on the database might have issues.")
-    LOGS.error(str(e))
+    LOGS.error("DB_URI is not configured. Features depending on the database might$    LOGS.error(str(e))
