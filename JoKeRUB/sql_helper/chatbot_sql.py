2538e765cab0d6de4ded5af58c77db0c4b3a4656
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-18 23:19:17 +0300

    Update chatbot_sql.py

--- JoKeRUB/sql_helper/chatbot_sql.py
+++ JoKeRUB/sql_helper/chatbot_sql.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, UnicodeText
 
-from . import BASE, SESSION
+from . import BASE, SESSION, engine
 
 
 class ChatBot(BASE):
@@ -30,7 +30,7 @@ class ChatBot(BASE):
         )
 
 
-ChatBot.__table__.create(checkfirst=True)
+ChatBot.__table__.create(bind=engine, checkfirst=True)
 
 
 def is_added(chat_id, user_id):
