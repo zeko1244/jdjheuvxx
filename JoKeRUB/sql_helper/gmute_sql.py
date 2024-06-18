1af767d1514ce3825499ed91d41b71722cdcc79d
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-18 23:24:33 +0300

    Update gmute_sql.py

--- JoKeRUB/sql_helper/gmute_sql.py
+++ JoKeRUB/sql_helper/gmute_sql.py
@@ -1,5 +1,5 @@
 try:
-    from . import BASE, SESSION
+    from . import BASE, SESSION, engine
 except ImportError as e:
     raise Exception("Hello!") from e
 
@@ -14,7 +14,7 @@ class GMute(BASE):
         self.sender = str(sender)
 
 
-GMute.__table__.create(checkfirst=True)
+GMute.__table__.create(bind=engine, checkfirst=True)
 
 
 def is_gmuted(sender_id):
