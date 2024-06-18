dac0bde957f5ec3befb6daf16782f775daef970d
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-18 23:13:10 +0300

    Update bot_blacklists.py

--- JoKeRUB/sql_helper/bot_blacklists.py
+++ JoKeRUB/sql_helper/bot_blacklists.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, String, UnicodeText
 
-from . import BASE, SESSION
+from . import BASE, SESSION, engine
 
 
 class Bot_BlackList(BASE):
@@ -22,7 +22,7 @@ class Bot_BlackList(BASE):
         return "<BL %s>" % self.chat_id
 
 
-Bot_BlackList.__table__.create(checkfirst=True)
+Bot_BlackList.__table__.create(bind=engine, checkfirst=True)
 
 
 def add_user_to_bl(
