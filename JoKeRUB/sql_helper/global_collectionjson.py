841f09627823c044b5ff82395d3980c51128eb60
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-18 23:23:02 +0300

    Update global_collectionjson.py

--- JoKeRUB/sql_helper/global_collectionjson.py
+++ JoKeRUB/sql_helper/global_collectionjson.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, UnicodeText
 from sqlalchemy_json import MutableJson, NestedMutableJson
 
-from . import BASE, SESSION
+from . import BASE, SESSION, engine
 
 
 class Cat_GlobalCollection_Json(BASE):
@@ -16,7 +16,7 @@ class Cat_GlobalCollection_Json(BASE):
         self.njson = njson
 
 
-Cat_GlobalCollection_Json.__table__.create(checkfirst=True)
+Cat_GlobalCollection_Json.__table__.create(bind=engine, checkfirst=True)
 
 
 def get_collection(keywoard):
