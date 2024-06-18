7e448f9fc7c421d070bba5a927f06122fcd90c3e
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-18 19:15:51 +0300

    Update __init__.py

--- JoKeRUB/sql_helper/__init__.py
+++ JoKeRUB/sql_helper/__init__.py
@@ -9,20 +9,17 @@ from ..core.logger import logging
 
 LOGS = logging.getLogger(__name__)
 
+BASE = declarative_base()
+
 def start() -> scoped_session:
     engine = create_engine(Config.DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-
 try:
-    BASE = declarative_base()
     SESSION = start()
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
-    LOGS.error(
-        "DB_URI is not configured. Features depending on the database might have issues."
-    )
+    LOGS.error("DB_URI is not configured. Features depending on the database might have issues.")
     LOGS.error(str(e))
-
