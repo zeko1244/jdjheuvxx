cb4224c6877adb152720739c41c393f1a22c4097
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-18 23:26:29 +0300

    Update locks_sql.py

--- JoKeRUB/sql_helper/locks_sql.py
+++ JoKeRUB/sql_helper/locks_sql.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Boolean, Column, String
 
-from . import BASE, SESSION
+from . import BASE, SESSION, engine
 
 
 class Locks(BASE):
@@ -22,7 +22,7 @@ class Locks(BASE):
         self.url = False
 
 
-Locks.__table__.create(checkfirst=True)
+Locks.__table__.create(bind=engine, checkfirst=True)
 
 
 def init_locks(chat_id, reset=False):
