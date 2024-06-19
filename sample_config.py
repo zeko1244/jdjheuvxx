67e0f5aee3322fa9e4431b5f669bd9562e3b5466
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-20 00:45:30 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -103,7 +103,7 @@ class Config(object):
         "THUMB_IMAGE", "https://telegra.ph/file/ca95524e4734b0d5461b5.jpg"
     )
     # specify NO_LOAD with plugin names for not loading in jepthon
-    NO_LOAD = [x for x in os.environ.get("NO_LOAD", "").split()]
+    NO_LOAD = list(os.environ.get("NO_LOAD", "").split())
     # for custom pic for .digitalpfp
     DIGITAL_PIC = os.environ.get("DIGITAL_PIC", None)
     DIGITAL_PIC_COLOR = os.environ.get("DIGITAL_PIC_COLOR", None)
