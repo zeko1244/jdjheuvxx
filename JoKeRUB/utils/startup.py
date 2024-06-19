c37bf27e86c37b68757d25f09a445793c9d77a74
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-20 00:27:32 +0300

    Update startup.py

--- JoKeRUB/utils/startup.py
+++ JoKeRUB/utils/startup.py
@@ -32,7 +32,7 @@ logging.getLogger('telethon').setLevel(logging.WARNING)
 ##Reda hands here
 cmdhr = Config.COMMAND_HAND_LER
 bot = l313l
-ENV = bool(os.environ.get("ENV", False))
+ENV = True
 
 if ENV:
     VPS_NOLOAD = ["سيرفر"]
