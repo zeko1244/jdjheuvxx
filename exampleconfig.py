59daa2d4d9668fb0985cebfad6c8d362cea3d02e
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-20 14:28:55 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,8 +1,8 @@
 from sample_config import Config
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 6
-    API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+    APP_ID = Your value
+    API_HASH = "Your Value"
     # the name to display in your alive message
     ALIVE_NAME = "Your value"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
