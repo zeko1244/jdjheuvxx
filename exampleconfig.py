c5a163bed6a30d8d397869e73fac21325774ec80
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-20 12:48:45 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -13,8 +13,6 @@ class Development(Config):
     STRING_SESSION = "Your value"
     # create a new bot in @botfather and fill the following vales with bottoken and username respectively
     TG_BOT_TOKEN = "Your value"
-    # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -100
     # command handler
     COMMAND_HAND_LER = "."
     # sudo enter the id of sudo users userid's in that array
