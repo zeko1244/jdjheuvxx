02454d06a02f755155f0d065eee763028bddcca2
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-20 12:10:41 +0300

    Update __init__.py

--- JoKeRUB/sql_helper/__init__.py
+++ JoKeRUB/sql_helper/__init__.py
@@ -1,16 +1,5 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~# CatUserBot #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-# Copyright (C) 2020-2023 by TgCatUB@Github.
-
-# This file is part of: https://github.com/TgCatUB/catuserbot
-# and is released under the "GNU v3.0 License Agreement".
-
-# Please see: https://github.com/TgCatUB/catuserbot/blob/master/LICENSE
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
 import os
-
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine                                               from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 # the secret configuration specific things
@@ -18,26 +7,18 @@ from ..Config import Config
 from ..core.logger import logging
 
 LOGS = logging.getLogger(__name__)
-
+engine = None
+BASE = declarative_base()
 
 def start() -> scoped_session:
-    database_url = (
-        Config.DB_URI.replace("postgres:", "postgresql:")
-        if "postgres://" in Config.DB_URI
-        else Config.DB_URI
-    )
-    engine = create_engine(database_url)
+    global engine
+    engine = create_engine(Config.DB_URI)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-
 try:
-    BASE = declarative_base()
-    SESSION = start()
+    SESSION = start()                                                              
 except AttributeError as e:
     # this is a dirty way for the work-around required for #23
-    LOGS.error(
-        "DB_URI is not configured. Features depending on the database might have issues."
-    )
-    LOGS.error(str(e))
+    LOGS.error("DB_URI" is not configured. Features depending on the database might$    LOGS.error(str(e))
