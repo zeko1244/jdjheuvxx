02cf7f1501aaa028c12e6df64cfdc2af0984f4be
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-26 01:37:23 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,16 +1,16 @@
 from sample_config import Config
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = Your value
-    API_HASH = "Your Value"
+    APP_ID = خلي ايبي ايدي هنا
+    API_HASH = "الايبي هاش"
     # the name to display in your alive message
-    ALIVE_NAME = "Your value"
+    ALIVE_NAME = "اسمك ع التليجرام"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "Your value"
+    DB_URI = "رابط قاعدة البيانات"
     # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
-    STRING_SESSION = "Your value"
+    STRING_SESSION = "كود التيرمكس"
     # create a new bot in @botfather and fill the following vales with bottoken and username respectively
-    TG_BOT_TOKEN = "Your value"
+    TG_BOT_TOKEN = "توكن البوت"
     # command handler
     COMMAND_HAND_LER = "."
     # sudo enter the id of sudo users userid's in that array
