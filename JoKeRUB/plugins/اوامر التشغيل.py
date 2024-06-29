7227bd907a847cc319858c6957496e1bdcfdae8e
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-29 23:30:41 +0300

    Update ⁨اوامر التشغيل.py⁩

--- JoKeRUB/plugins/اوامر التشغيل.py
+++ JoKeRUB/plugins/اوامر التشغيل.py
@@ -118,7 +118,7 @@ async def Hussein(event):
 async def Hussein(event):
     if event.reply_to and event.sender_id in JOKRDEV:
         reply_msg = await event.get_reply_message()
-        owner_id = reply_msg.from_id.user_id
+        owner_id = reply_msg.from_id
         if owner_id == l313l.uid:
             if event.message.message == "اطفاء":
                     await event.reply("**᯽︙ تدلل مولاي تم اطفاء السورس بواسطة تاج راسك 😁**")
