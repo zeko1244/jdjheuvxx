26390ee1d39e2601a208e96c069a10696dc93793
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-30 17:25:39 +0300

    Update ⁨التخزين.py⁩

--- JoKeRUB/plugins/التخزين.py
+++ JoKeRUB/plugins/التخزين.py
@@ -55,8 +55,8 @@ async def monito_p_m_s(event):  # sourcery no-metrics
                         )
                     LOG_CHATS_.COUNT = 0
                 
-                original_message = f"الرسالة الاصلية: {event.original_update.message.text}" if event.original_update and isinstance(event.original_update.message, Message) else "الرسالة الاصلية: N/A"
-                edited_message = f"الرسالة المعدلة: {event.message.text}" if isinstance(event.message, Message) else "الرسالة المعدلة: N/A"
+                original_message = f"الرسالة الاصلية: {event.message.text}" if isinstance(event.message, Message) else "الرسالة الاصلية: N/A"
+                edited_message = f"الرسالة المعدلة: {event.message.text}"
                 
                 LOG_CHATS_.NEWPM = await event.client.send_message(
                     Config.PM_LOGGER_GROUP_ID,
