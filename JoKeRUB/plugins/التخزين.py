a4bd2e166caca952a0cbf0f3afc877f3cb3625dc
Jepthon Arabic <91113247+almul8ab@users.noreply.github.com>
2024-06-30 16:52:50 +0300

    Update ⁨التخزين.py⁩

--- JoKeRUB/plugins/التخزين.py
+++ JoKeRUB/plugins/التخزين.py
@@ -25,6 +25,7 @@ class LOG_CHATS:
 
 LOG_CHATS_ = LOG_CHATS()
 
+
 @l313l.ar_cmd(incoming=True, func=lambda e: e.is_private, edited=True, forword=None)
 async def monito_p_m_s(event):  # sourcery no-metrics
     if Config.PM_LOGGER_GROUP_ID == -100:
@@ -67,8 +68,8 @@ async def monito_p_m_s(event):  # sourcery no-metrics
 
             if event.edit_date:  # Check if the message is edited
                 try:
-                    original_message = await event.get_reply_message()
-                    original_text = original_message.text if original_message else "N/A"
+                    original_message = await event.client.get_messages(event.chat_id, ids=[event.original_message.id])
+                    original_text = original_message[0].text if original_message else "N/A"
                     new_text = f"لقد قام بتعديل الرسالة\nالرسالة الاصلية: {original_text}\nالرسالة المعدلة: {event.message.text}"
                     await event.client.send_message(
                         Config.PM_LOGGER_GROUP_ID,
